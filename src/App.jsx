4cd5bfcc0e329c84175d01cb3e3a2612fd2a005d
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-26 15:17:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import articles from './data/articles'
+
+function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/data/articles.js
@@ -0,0 +1,24 @@
+const articles = [
+    {
+        id: 1,
+        title: "Le migliori tecniche di dribbling nel calcio"
+    },
+    {
+        id: 2,
+        title: "Analisi tattica delle squadre di Serie A"
+    },
+    {
+        id: 3,
+        title: "Storia delle Coppe Europee: un viaggio nel passato"
+    },
+    {
+        id: 4,
+        title: "I più grandi allenatori di tutti i tempi"
+    },
+    {
+        id: 5,
+        title: "L'evoluzione delle regole del calcio nel corso degli anni"
+    }
+];
+
+export default articles
